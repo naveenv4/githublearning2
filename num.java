e301224ebc00dba41d0f11b06a7920c9838b7d0b
Naveen <naveenmacle@gmail.com>
2025-12-05 20:37:08 +0530

    created num and hello files

---
+++ num.java
@@ -0,0 +1,8 @@
+public class num{
+    public static void main(String args[]){
+        int a=5;
+        int b=10;
+        int c = a + b;
+        System.out.println("Sum of a and b is : "+c);
+    }
+}
